0aac60099c3ebf061da671dc254d97d570c4ba6b
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-29 00:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  const panels = document.querySelectorAll(".panel");
+
+  panels.forEach(panel => {
+    panel.addEventListener("click", function() {
+      panels.forEach(p => {
+        p.classList.remove("active");
+      });
+      this.classList.add("active");
+    });
+  });
+});
